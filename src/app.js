ea8da579da05d2a859c576ca132f75c5e19c7e79
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-05-30 18:53:54 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensiones = [".com", ".es", ".net"];
+  let vueltas = 0;
+  let tag = document.querySelector("#dominios");
+  let dominios = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let h = 0; h < noun.length; h++) {
+        for (let g = 0; g < extensiones.length; g++) {
+          dominios +=
+            "<li class='list-group-item'>" +
+            pronoun[i] +
+            adj[j] +
+            noun[h] +
+            extensiones[g] +
+            "</li>";
+          tag.innerHTML = dominios;
+        }
+      }
+    }
+    vueltas++;
+  }
   console.log("Hello Rigo from the console!");
 };
